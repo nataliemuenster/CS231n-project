64d2c82fbba9082d36ddfe0406c67d2d931d63b4
Shrey Gupta <shreyg19@stanford.edu>
2018-06-02 23:44:08 +0000

    Added a small utility script to help with disk resizing for the GCE VM.

---
+++ data-utils/resize-disk.sh
@@ -0,0 +1,14 @@
+echo "Displaying current block devices and disk sizes." 
+lsblk
+read -p "Does sda have the size you inputted on the GCE console? (y/n)" -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Growing partition for /dev/sda1. (If disk is full, this command will fail.)"
+    sudo growpart /dev/sda 1
+    echo "Resizing filesystem to size of partititon."
+    sudo resize2fs /dev/sda1
+    df -h
+else
+    echo "Aborting program. Check the GCE console to confirm increase in disk size."
+fi
+
